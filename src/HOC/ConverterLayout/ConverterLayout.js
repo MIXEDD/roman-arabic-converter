c5507da2eda1764bac04158bedf6b521d1dd4cfe
Maksimilian Ceresniovyj <maxas81@gmail.com>
2019-07-02 11:43:15 +0300

    prep work

---
+++ src/HOC/ConverterLayout/ConverterLayout.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Converter from '../../components/Converter/Converter';
+
+
+class ConverterLayout extends Component{
+
+    render() {
+        return(
+            <Converter/>
+        );
+    }
+}
+
+export default ConverterLayout;
---
+++ src/components/Converter/Converter.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Converter.css';
+
+class Converter extends Component{
+
+    render() {
+        return(
+            <div className="converter">
+                
+            </div>
+        );
+    }
+}
+
+export default Converter;
